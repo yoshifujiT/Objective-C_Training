1c558c2f26c8721eca06b931b8e0173f38a7c033
yoshifuji <t.yoshifuji@me.com>
2021-04-04 12:26:39 +0900

    Initial Commit

---
+++ Objetive-C_Training/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objetive-C_Training
+//
+//  Created by ToruYoshifuji on 2021/04/04.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Objective-C_Training
+Objective-C Tutorial
+
+## Refs
+https://www.tuyano.com/index2?id=524006
